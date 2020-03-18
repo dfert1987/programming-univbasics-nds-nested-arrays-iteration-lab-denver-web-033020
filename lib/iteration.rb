c643d4515f9d85cc21f4f1b66795182dc1128439
David Fertitta <davidj.fertitta@gmail.com>
2020-03-18 19:32:49 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -36,8 +36,8 @@ def total_even_pairs(src)
   row_index = 0
   while row_index < src.count do 
     element_index = 0 
-    if src[row_index][0] % 2 == 0 && src[row_index][1] % 2 == 0
-      evens_total += src[row_index][0] + src[row_index][1]
+    if src[row_index][0] % 2 == 0 && src[row_index][1] % 2 == 0 
+      evens_total += src[row_index][0] + src[row_index][1] % 2 == 0 
   end
     row_index += 1 
   end
